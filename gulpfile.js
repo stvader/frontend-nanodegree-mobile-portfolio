e14843b1ac9068db3f41e9aa73f8520567d685d9
Vadim St <vadim_st_13@mail.ru>
2017-09-20 09:43:14 +0300

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync");
+//var less = require('gulp-less');
+//var plumber = require("gulp-plumber");
+//var postcss = require("gulp-postcss");
+//var autoprefixer = require("autoprefixer");
+//var server = require("browser-sync");
 var rename = require('gulp-rename');
-var mqpacker = require('css-mqpacker');
-var minify = require('gulp-csso');
-var imagemin = require('gulp-imagemin');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
+//var mqpacker = require('css-mqpacker');
+//var minify = require('gulp-csso');
+//var imagemin = require('gulp-imagemin');
+//var svgstore = require('gulp-svgstore');
+//var svgmin = require('gulp-svgmin');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('style', function() {
 	gulp.src('less/style.less')
@@ -48,24 +49,18 @@ gulp.task('server', ["style"], function() {
 });
 
 gulp.task('stylemin', function() {
-	gulp.src('less/style.less')
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(postcss([
-			autoprefixer({browsers: [
-				"last 1 version",
-				"last 2 Chrome version",
-				"last 2 Firefox version",
-				"last 2 Opera version",
-				"last 2 Edge version"
-			]}),
-			mqpacker({
-				sort: true
-			})
-		]))
+	gulp.src('css/style.css')		
 		.pipe(gulp.dest('css'))
-		.pipe(minify())
+		.pipe(cssmin())
 		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('styleprintmin', function() {
+	gulp.src('css/style.css')		
+		.pipe(gulp.dest('css'))
+		.pipe(cssmin())
+		.pipe(rename('print.min.css'))
 		.pipe(gulp.dest('css'));
 });
 
@@ -78,12 +73,3 @@ gulp.task('images', function(){
 		.pipe(gulp.dest('img'));
 });
 
-gulp.task('symbols', function() {
-	return gulp.src('img/icon/*.svg')
-		.pipe(svgmin())
-		.pipe(svgstore({
-			inlineSvg: true
-		}))
-		.pipe(rename('symbols.svg'))
-		.pipe(gulp.dest('img'));
-});
