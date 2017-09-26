348c8a25e7de7718afa48099284b41991718129e
Vadim St <vadim_st_13@mail.ru>
2017-09-26 19:45:25 +0300

    attempt cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,5 @@
 var gulp = require('gulp');
-//var less = require('gulp-less');
-//var plumber = require("gulp-plumber");
-//var postcss = require("gulp-postcss");
-//var autoprefixer = require("autoprefixer");
-//var server = require("browser-sync");
 var rename = require('gulp-rename');
-//var mqpacker = require('css-mqpacker');
-//var minify = require('gulp-csso');
-//var imagemin = require('gulp-imagemin');
-//var svgstore = require('gulp-svgstore');
-//var svgmin = require('gulp-svgmin');
 var cssmin = require('gulp-cssmin');
 var htmlmin = require('gulp-htmlmin');
 
